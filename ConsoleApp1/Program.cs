29d27ed3e4599eb60f3ab3579d16ae7f86f9f67a
Thomas Castiglione <thomas@agcom.com.au>
2017-08-30 18:42:14 +0800

    create minimal repro

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    class A { public override string ToString() => "A"; }
+    class B { public override string ToString() => "B"; }
+    class C { public override string ToString() => "C"; }
+
+    static void Main(string[] args)
+    {
+        var results = new List<A> { new A() }
+            .Concat(new List<object> { new B() })
+            .Concat(new List<C> { new C() })
+            .ToArray();
+
+        Console.WriteLine(string.Join(' ', results));
+    }
+}
